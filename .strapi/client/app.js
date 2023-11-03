ec2e146bb41299d0201e5aefcafb351bc637632b
BuhayovA <neznaykagm@gmail.com>
2023-11-03 10:19:40 +0200

    fix

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -5,7 +5,6 @@
 import contentTypeBuilder from "@strapi/plugin-content-type-builder/strapi-admin";
 import email from "@strapi/plugin-email/strapi-admin";
 import upload from "@strapi/plugin-upload/strapi-admin";
-import ckeditor from "../../src/plugins/strapi-plugin-ckeditor/strapi-admin";
 import i18N from "@strapi/plugin-i18n/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
 import { renderAdmin } from "@strapi/admin/strapi-admin";
@@ -15,7 +14,6 @@ renderAdmin(document.getElementById("strapi"), {
     "content-type-builder": contentTypeBuilder,
     email: email,
     upload: upload,
-    ckeditor: ckeditor,
     i18n: i18N,
     "users-permissions": usersPermissions,
   },
